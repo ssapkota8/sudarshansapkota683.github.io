8804e1aae1fa21a74b8c06aa279c5c1f3c7ba445
Sudarshan Sapkota <sudarshanofficial683@gmail.com>
2025-05-01 02:39:06 -0400

    Delete src directory

--- src/pages/Contact.tsx
+++
@@ -1,9 +0,0 @@
-export default function Contact() {
-  return (
-    <section className="p-8">
-      <h2 className="text-3xl font-bold mb-4">Contact</h2>
-      <p>Email: sudarshansapkota86@gmail.com</p>
-      <p>LinkedIn: linkedin.com/in/sudarshansapkota</p>
-    </section>
-  );
-}
