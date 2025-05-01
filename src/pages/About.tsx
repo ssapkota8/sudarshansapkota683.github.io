093a50d59927965d86f47c46d8a919a49cf2f0ab
Sudarshan Sapkota <sudarshanofficial683@gmail.com>
2025-05-01 02:21:32 -0400

    Create About.tsx

---
+++ src/pages/About.tsx
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <section className="p-8">
+      <h2 className="text-3xl font-bold mb-4">About Me</h2>
+      <p>
+        I recently graduated from Seneca College in Computer Systems Technology. I’m skilled in cloud platforms like AWS & Azure and have experience in web development, cybersecurity, and system automation.
+      </p>
+    </section>
+  );
+}
